7c4829b73889b130439e4ad0067ad59fe33db976
leanderegg <leanderegg@gmail.com>
2022-06-01 22:16:10 -0700

    imported new sapflow data

--- SensorDataCleaning.R
+++ SensorDataCleaning.R
@@ -24,7 +24,7 @@ library(here) #for easier/less buggy file organization
 source("/Users/leeanderegg/Desktop/R functions, general/ggplot_helpers.R")
 
 #______________________________________________________________
-############### Begin: SAPFLOW #######################################
+############### Begin: SAPFLOW - Load and Clean #######################################
 #______________________________________________________________
 
 sapflow.dir <- "Data_05042022/sapflow/"
@@ -34,7 +34,8 @@ sapfiles <- dir(sapflow.dir)
 #### RT sapflow  ###########
 start.date <- as_datetime("2022-03-24 17:24:33") # ED04 deployment date
 RTtree.names <- data.frame(Sensor=c("SAP.FLOW.TOTAL","SAP.FLOW.TOTAL.1","SAP.FLOW.TOTAL.2","SAP.FLOW.TOTAL.3"), Tree=c("2381","2384","2382","2010"))
-RT <- read.csv("Data_05042022/sapflow/20220331-101209 Log Download_ED04.csv", header=T, na.strings = "-1004600",skip = 3)
+#RT <- read.csv("Data_05042022/sapflow/20220331-101209 Log Download_ED04.csv", header=T, na.strings = "-1004600",skip = 3)
+RT <- read.csv("Data_05042022/sapflow/20220530-111350 Log Download_ED04.csv", header=T, na.strings = "-1004600",skip = 3)
 RT <- RT[-1,]
 colnames(RT)[1] <- "Date.Time"
 RT$Date.Time  <- lubridate::as_datetime(RT$Date.Time,format="%d/%m/%y %H:%M:%S")
@@ -64,6 +65,7 @@ RT.dendro$Growth_um <- RT.dendro$rawRadius - min(RT.dendro$rawRadius, na.rm=T)
 RT.dendro$Growth_sc <- RT.dendro$Growth_um/max(RT.dendro$Growth_um,na.rm=T)  
 #kill the dendrometer readings
 RT.long <- RT.long[which(RT.long$Sensor!="S1Radius"),]
+RT.long$Install <- 1
 # label RT
 RT.long$Loc <- "RT"
 
@@ -88,6 +90,7 @@ RT.old.long <- pivot_longer(RT.old.wide, names_to="Sensor",col=-1, values_to = "
 
 # name the trees
 RT.old.long$Tree <- RTtree.names$Tree[match(RT.old.long$Sensor, RTtree.names$Sensor)]
+RT.old.long$Install <- 1
 # label RT
 RT.old.long$Loc <- "RT"
 
@@ -97,9 +100,13 @@ RT.old.long$Loc <- "RT"
 start.date <- as_date("2022-02-18") # ED01 deployment date
 reprog.date <- as_datetime("17/03/22 11:32:28",format="%d/%m/%y %H:%M:%S") # date.time of reprogramming to add sensor 4
 sensor.switch.date <- reprog.date # also switched sensor 3 from one tree to another on this date
+sensor.check1 <- as_datetime("20/04/22 13:05:28",format="%d/%m/%y %H:%M:%S") # datetime when probe 3 became same tree as probe 1 and probe 2 became new install (technically a 1:25 not 1:05 for probe 2 but whatever)
+sensor.check2 <-as_datetime("27/04/22 11:32:28",format="%d/%m/%y %H:%M:%S") # date when probe 1 -> new install (where probe 4 was) and probe 3 became old install of probe 2 tree
+sensor.check3 <-as_datetime("04/05/22 11:32:28",format="%d/%m/%y %H:%M:%S") # date when probe 3 went back to being probe 3
 TStree.names <- data.frame(Sensor=c("SAP.FLOW.TOTAL","SAP.FLOW.TOTAL.1","SAP.FLOW.TOTAL.2","SAP.FLOW.TOTAL.3"), Tree=c("2347","2346","2349","2343"))
 
-TS <- read.csv("Data_05042022/sapflow/20220331-130401 Log Download_ED01.csv", header=T, na.strings = c("-1004600","-99.000"),skip = 3)
+#TS <- read.csv("Data_05042022/sapflow/20220331-130401 Log Download_ED01.csv", header=T, na.strings = c("-1004600","-99.000"),skip = 3)
+TS <- read.csv("Data_05042022/sapflow/20220530-123409 Log Download_ED01.csv", header=T, na.strings = c("-1004600","-99.000"),skip = 3)
 TS <- TS[-1,]
 colnames(TS)[1] <- "Date.Time"
 TS$Date.Time  <- lubridate::as_datetime(TS$Date.Time,format="%d/%m/%y %H:%M:%S")
@@ -123,8 +130,21 @@ TS.long <- pivot_longer(TS.wide, names_to="Sensor",col=-1, values_to = "Sapflow"
 
 # name the trees
 TS.long$Tree <- TStree.names$Tree[match(TS.long$Sensor, TStree.names$Sensor)]
+# add in a new column for install location (since now multiple per tree)
+TS.long$Install <- 1
   # switch sensor 3 on reprog.date
 TS.long$Tree[which(TS.long$Sensor=="SAP.FLOW.TOTAL.2" & TS.long$Date.Time>sensor.switch.date)] <- "2009"
+# switch sensor 3 on sensor check1 date
+TS.long$Tree[which(TS.long$Sensor=="SAP.FLOW.TOTAL.2" & TS.long$Date.Time>sensor.check1 & TS.long$Date.Time < sensor.check2)] <- "2347" # for new install to check probe 1
+TS.long$Install[which(TS.long$Sensor=="SAP.FLOW.TOTAL.2" & TS.long$Date.Time>sensor.check1 & TS.long$Date.Time < sensor.check2)] <- 2 # for new install location on tree 2347
+# switch sensor 2 to new location on sensor check1 date
+TS.long$Install[which(TS.long$Sensor=="SAP.FLOW.TOTAL.1" & TS.long$Date.Time>sensor.check1)] <- 2 # for new install location
+# switch sensor 3 to new location on sensor.check2 date, but install location 1
+TS.long$Tree[which(TS.long$Sensor=="SAP.FLOW.TOTAL.2" & TS.long$Date.Time>sensor.check2 & TS.long$Date.Time < sensor.check3)] <- "2346" # for new install in 2346
+# finally switch back to 2009 install 1 sensor.check3
+TS.long$Tree[which(TS.long$Sensor=="SAP.FLOW.TOTAL.2" & TS.long$Date.Time>sensor.check3)] <- "2009" # for return to original tree
+
+
 # label TS
 TS.long$Loc <- "TS"
 
@@ -135,7 +155,9 @@ start.date <- as_date("03/03/22 11:29:54", format="%d/%m/%y %H:%M:%S") # ED01 de
 reprog.date <- as_datetime("26/03/22 17:23:10",format="%d/%m/%y %H:%M:%S") # date.time of reprogramming to add sensor 4
 MStree.names <- data.frame(Sensor=c("SAP.FLOW.TOTAL","SAP.FLOW.TOTAL.1","SAP.FLOW.TOTAL.2","SAP.FLOW.TOTAL.3"), Tree=c("2369","2367","2365","2360"))
 
-MS <- read.csv("Data_05042022/sapflow/20220331-111140 Log Download_ED02.csv", header=T, na.strings = c("-1004600","-99.000"),skip = 3)
+#MS <- read.csv("Data_05042022/sapflow/20220331-111140 Log Download_ED02.csv", header=T, na.strings = c("-1004600","-99.000"),skip = 3)
+MS <- read.csv("Data_05042022/sapflow/20220530-103346 Log Download_ED02.csv", header=T, na.strings = c("-1004600","-99.000"),skip = 3)
+
 MS <- MS[-1,]
 colnames(MS)[1] <- "Date.Time"
 MS$Date.Time  <- lubridate::as_datetime(MS$Date.Time,format="%d/%m/%y %H:%M:%S")
@@ -159,6 +181,7 @@ MS.long <- pivot_longer(MS.wide, names_to="Sensor",col=-1, values_to = "Sapflow"
 
 # name the trees
 MS.long$Tree <- MStree.names$Tree[match(MS.long$Sensor, MStree.names$Sensor)]
+MS.long$Install <- 1
 # label MS
 MS.long$Loc <- "MS"
 
@@ -171,8 +194,27 @@ sap$Sapflow.st <- sap$Sapflow/sapmax$maxSapflow[match(sap$Tree, sapmax$Tree)]
 sap$Loc <- factor(sap$Loc)
 
 
-###New dataset: 
-write_csv(sap, here("processed-data", "sapflow.csv"))
+
+### Write out processed Sapflow: 
+write_csv(sap, here("processed-data", "sapflow_06012022.csv"))
+  # versioned this in case we overwrite old files
+
+
+#______________________________________________________________
+############### END: Load and Clean #######################################
+#______________________________________________________________
+
+
+
+
+
+
+#______________________________________________________________
+############### Begin: Visualize Sapflow #######################################
+#______________________________________________________________
+
+
+
 
 
 plot(Sapflow.st~Date.Time, sap, col=factor(Loc), pch=".", ylim=c(-.5,1))
@@ -182,7 +224,7 @@ for(i in unique(sap$Tree)){
 abline(h=0)
 
 quartz(width=6, height=3)
-ggplot(sap[which(sap$Tree==2009),], aes(x=Date.Time, y=Sapflow, col=Tree)) + geom_line() +geom_hline(yintercept = 0)+ facet_wrap(~Loc)  +
+ggplot(sap[which(sap$Tree==2384),], aes(x=Date.Time, y=Sapflow, col=Tree)) + geom_line() +geom_hline(yintercept = 0)+ facet_wrap(~Loc)  +
   geom_line(data=RT.dendro, aes(y=Growth_sc)) # add dendrometer
 
 
@@ -195,6 +237,27 @@ abline(v=as_datetime("2022-03-28 12:00:00"), col="blue") # rain event
 plot(Sapflow~Date.Time, RT.long, col=factor(Tree), pch=".")
 abline(h=0)
 lines(Growth_sc~Date.Time, RT.dendro, col="red")
+
+
+
+
+
+#######3 . Examinging ED01 problems ##########3
+
+#sensor 1 actually really came to the party, it must have been weird sapflow early on.
+#it's super noisy, so probably best to switch to install location2, but we can definitely inter-relate the two
+ggplot(TS.long[TS.long$Tree %in% c("2347","2346") & TS.long$Date.Time>(sensor.check1),], aes(x=Date.Time, y=Sapflow, col=Sensor)) + geom_line()
+
+plot(SAP.FLOW.TOTAL~SAP.FLOW.TOTAL.2,TS.wide[TS.wide$Date.Time>sensor.check1,])
+abline(h=0)
+abline(v=0)
+abline(lm(SAP.FLOW.TOTAL~SAP.FLOW.TOTAL.2,TS.wide[TS.wide$Date.Time>sensor.check1,]))
+
+
+# sensor 2 - it actually doesn't look that bad, in the last few days of the record
+# but something happened in the off time, and it got real low again...
+ggplot(TS.long[TS.long$Tree %in% c("2347","2346") & TS.long$Date.Time>as_date("2022-04-13"),], aes(x=Date.Time, y=Sapflow, col=Sensor)) + geom_line()
+ggplot(TS.long[TS.long$Tree %in% c("2347","2346") & TS.long$Date.Time>as_date("2022-03-13"),], aes(x=Date.Time, y=Sapflow, col=Sensor)) + geom_line()
 
 
 
