2269a354befde0276341ba9f6874ec60e474ef31
leanderegg <leanderegg@gmail.com>
2022-07-28 13:32:05 -0700

    updated data version

--- Cd-WaterPotentialCleaning.R
+++ Cd-WaterPotentialCleaning.R
@@ -15,6 +15,7 @@ library(sf)
 library(raster)
 
 # some namespace issue with raster and some of these packages.
+library(dplyr)
 library(tidyverse)
 library(readxl)
 library(car)
@@ -36,6 +37,10 @@ ggplot <- function(...) { ggplot2::ggplot(...) +
 
 #_______________________________________________________________
 ############# LOAD DATA ########################################
+
+### Water potential version #####
+# note: this currently assumes that everything is stored in Data_05042022/ with versioning in the file name
+wpversion <- "SHIFT data collection 2022_22.06.27.xlsx"
 
 ###### Sedgwick DEM
 
@@ -64,30 +69,31 @@ latlon.ll <- SpatialPointsDataFrame(coords = data.frame(latlon$Longitude, latlon
 latlon.utm <- spTransform(latlon.ll, CRSobj = crs(sdem))
 
 
-## table for linking tree numbers with sites
-treesite <- read_excel("Data_05042022/WP_WC/SHIFT data collection 2022.xlsx",sheet="Tree_Sites", skip=0, na = "NA")
 
+## table for linking tree numbers with sites
+treesite <- read_excel(paste0("Data_05042022/WP_WC/",wpversion),sheet="Tree_Sites", skip=0, na = "NA", col_types = "text")
+treesite$Tag <- str_replace(treesite$Tag, "\\.0","")
 
 ####### Water Potentials 
 #Date: 228 WP + LWC
-wpwc228 <- read_excel("Data_05042022/WP_WC/SHIFT data collection 2022.xlsx",sheet="228 WP + LWC", skip=5, na = "NA")
+wpwc228 <- read_excel(paste0("Data_05042022/WP_WC/",wpversion),sheet="228 WP + LWC", skip=5, na = "NA")
 
 #Date: 38-311 WP + LWC
-wpwc38 <- read_excel("Data_05042022/WP_WC/SHIFT data collection 2022.xlsx",sheet="38-311 WP + LWC", skip=5, na = "NA")
+wpwc38 <- read_excel(paste0("Data_05042022/WP_WC/",wpversion),sheet="38-311 WP + LWC", skip=5, na = "NA")
 # remove mislabeled tree 2567 which was actually 2367 
 wpwc38 <- wpwc38[-which(wpwc38$Tag=="2567"),]
 
 #Date: 315 WP + LWC
-wpwc315 <- read_excel("Data_05042022/WP_WC/SHIFT data collection 2022.xlsx",sheet="315 WP + LWC", skip=5, na = "NA")
+wpwc315 <- read_excel(paste0("Data_05042022/WP_WC/",wpversion),sheet="315 WP + LWC", skip=5, na = "NA")
 
 #Date: 330 WP + LWC (core)
-wpwc330 <- read_excel("Data_05042022/WP_WC/SHIFT data collection 2022.xlsx",sheet="330 WP + LWC", skip=5, na = "NA")
+wpwc330 <- read_excel(paste0("Data_05042022/WP_WC/",wpversion),sheet="330 WP + LWC", skip=5, na = "NA")
 
 #Date: 411-412 WP + LWC (satellite)
-wpwc44 <- read_excel("Data_05042022/WP_WC/SHIFT data collection 2022.xlsx",sheet="44 WP + LWC", skip=5, na = "NA")
+wpwc44 <- read_excel(paste0("Data_05042022/WP_WC/",wpversion),sheet="44 WP + LWC", skip=5, na = "NA")
 
 #Date: 411-412 WP + LWC
-wpwc411 <- read_excel("Data_05042022/WP_WC/SHIFT data collection 2022.xlsx",sheet="411-412 WP + LWC", skip=5, na = "NA")
+wpwc411 <- read_excel(paste0("Data_05042022/WP_WC/",wpversion),sheet="411-412 WP + LWC", skip=5, na = "NA")
 
 
 
@@ -97,14 +103,15 @@ wpwc411 <- read_excel("Data_05042022/WP_WC/SHIFT data collection 2022.xlsx",shee
 ######### . Data cleaning ####################
 
 # take wide form and hella messy data and start making in long
+# note, loading {raster} caused a problem with select() from dplyr. just defensively coded it.
 
 # pull out predawns and make them wide form
 ####### ++ 2.28.22 #########
 wp228pd <- wpwc228 %>%
   filter(!is.na(Tag)) %>%
-  select(!matches("_g_")) %>% 
-  select(1:3,6,matches("PD")) %>% 
-  select(-PD_bulk_wet, -PD_bulk_dry, -PD_avg) %>% 
+  dplyr::select(!matches("_g_")) %>% 
+  dplyr::select(1:3,6,matches("PD")) %>% 
+  dplyr::select(-PD_bulk_wet, -PD_bulk_dry, -PD_avg) %>% 
   pivot_longer(cols=matches("PD[1-9]") 
                , names_to="PD"
                , values_to="MPa"
@@ -116,9 +123,9 @@ wp228pd$Date <- as_date("2022-02-28")
 
 wp228md <- wpwc228 %>%
   filter(!is.na(Tag)) %>%
-  select(!matches("_g_")) %>% 
-  select(1:3,6,matches("MD")) %>% 
-  select(-MD_bulk_wet, -MD_bulk_dry, -MD_avg) %>% 
+  dplyr::select(!matches("_g_")) %>% 
+  dplyr::select(1:3,6,matches("MD")) %>% 
+  dplyr::select(-MD_bulk_wet, -MD_bulk_dry, -MD_avg) %>% 
   pivot_longer(cols=matches("MD[1-9]") 
                , names_to="MD"
                , values_to="MPa"
@@ -137,11 +144,11 @@ wp228mdind$Date.MD[which(is.na(wp228mdind$Date.MD))] <- as_date("2022-03-08") # 
 wp228mdind$WOY <- week(wp228mdind$Date.MD)
 
 
-#add in lat lon
+#Merge PD and MD
 wp228all <- full_join(wp228pdind, wp228mdind, by=c("Tag", "WOY"))
 
-
-wp228 <- left_join(wp228all, latlon, by= c("Tag"="Tree"))
+# add in lat lon
+#wp228 <- left_join(wp228all, latlon, by= c("Tag"="Tree"))
 #wp228$Edrop <- wp228$MD_MPa - wp228$PD_MPa
 
 
@@ -150,9 +157,9 @@ wp228 <- left_join(wp228all, latlon, by= c("Tag"="Tree"))
 ####### ++ 3.08.22 & 3.11.22 #########
 wp38pd <- wpwc38 %>%
   filter(!is.na(Tag)) %>%
-  select(!matches("_g_")) %>% 
-  select(1:3,6,matches("PD")) %>% 
-  select(-PD_bulk_wet, -PD_bulk_dry, -PD_avg) %>% 
+  dplyr::select(!matches("_g_")) %>% 
+  dplyr::select(1:3,6,matches("PD")) %>% 
+  dplyr::select(-PD_bulk_wet, -PD_bulk_dry, -PD_avg) %>% 
   pivot_longer(cols=matches("PD[1-9]") 
                , names_to="PD"
                , values_to="MPa"
@@ -165,9 +172,9 @@ wp38pd$Date <- as_date(wp38pd$Date, format="%m/%d/%Y")
 
 wp38md <- wpwc38 %>%
   filter(!is.na(Tag)) %>%
-  select(!matches("_g_")) %>% 
-  select(1:3,6,matches("MD")) %>% 
-  select(-MD_bulk_wet, -MD_bulk_dry, -MD_avg) %>% 
+  dplyr::select(!matches("_g_")) %>% 
+  dplyr::select(1:3,6,matches("MD")) %>% 
+  dplyr::select(-MD_bulk_wet, -MD_bulk_dry, -MD_avg) %>% 
   pivot_longer(cols=matches("MD[1-9]") 
                , names_to="MD"
                , values_to="MPa"
@@ -186,20 +193,20 @@ wp38mdind$Date.MD[which(is.na(wp38mdind$Date.MD))] <- as_date("2022-03-08") # mi
 wp38mdind$WOY <- week(wp38mdind$Date.MD)
 
 
-#add in lat lon
+#Merge PD and MD
 wp38all <- full_join(wp38pdind, wp38mdind, by=c("Tag", "WOY"))
 
 
-wp38 <- left_join(wp38all, latlon, by= c("Tag"="Tree"))
+#wp38 <- left_join(wp38all, latlon, by= c("Tag"="Tree"))
 #wp38$Edrop <- wp38$MD_MPa - wp38$PD_MPa
 
 
 ####### ++ 3.15.22 #########
 wp315pd <- wpwc315 %>%
   filter(!is.na(Tag)) %>%
-  select(!matches("_g_")) %>% 
-  select(1:3,6,matches("PD")) %>% 
-  select(-PD_bulk_wet, -PD_bulk_dry, -PD_avg) %>% 
+  dplyr::select(!matches("_g_")) %>% 
+  dplyr::select(1:3,6,matches("PD")) %>% 
+  dplyr::select(-PD_bulk_wet, -PD_bulk_dry, -PD_avg) %>% 
   pivot_longer(cols=matches("PD[1-9]") 
                , names_to="PD"
                , values_to="MPa"
@@ -211,9 +218,9 @@ wp315pd$Date <- as_date("2022-03-15")
 
 wp315md <- wpwc315 %>%
   filter(!is.na(Tag)) %>%
-  select(!matches("_g_")) %>% 
-  select(1:3,6,matches("MD")) %>% 
-  select(-MD_bulk_wet, -MD_bulk_dry, -MD_avg) %>% 
+  dplyr::select(!matches("_g_")) %>% 
+  dplyr::select(1:3,6,matches("MD")) %>% 
+  dplyr::select(-MD_bulk_wet, -MD_bulk_dry, -MD_avg) %>% 
   pivot_longer(cols=matches("MD[1-9]") 
                , names_to="MD"
                , values_to="MPa"
@@ -235,7 +242,7 @@ wp315mdind$WOY <- week(wp315mdind$Date.MD)
 wp315all <- full_join(wp315pdind, wp315mdind, by=c("Tag", "WOY"))
 
 
-wp315 <- left_join(wp315all, latlon, by= c("Tag"="Tree"))
+#wp315 <- left_join(wp315all, latlon, by= c("Tag"="Tree"))
 #wp315$Edrop <- wp315$MD_MPa - wp315$PD_MPa
 
 
@@ -245,9 +252,9 @@ wp315 <- left_join(wp315all, latlon, by= c("Tag"="Tree"))
 # currently just pd from core
 wp330pd <- wpwc330 %>%
   filter(!is.na(Tag)) %>%
-  select(!matches("_g_")) %>% 
-  select(1:3,6,matches("PD")) %>% 
-  select(-PD_bulk_wet, -PD_bulk_dry, -PD_avg) %>% 
+  dplyr::select(!matches("_g_")) %>% 
+  dplyr::select(1:3,6,matches("PD")) %>% 
+  dplyr::select(-PD_bulk_wet, -PD_bulk_dry, -PD_avg) %>% 
   pivot_longer(cols=matches("PD[1-9]") 
                , names_to="PD"
                , values_to="MPa"
@@ -258,9 +265,9 @@ wp330pd$Date <- as_date("2022-03-30")
 # currently no MD values
 # wp330md <- wpwc330 %>%
 #   filter(!is.na(Tag)) %>%
-#   select(!matches("_g_")) %>% 
-#   select(1:3,6,matches("MP")) %>% 
-#   select(-MP_bulk_wet, -MP_bulk_dry, -MP_avg) %>% 
+#   dplyr::select(!matches("_g_")) %>% 
+#   dplyr::select(1:3,6,matches("MP")) %>% 
+#   dplyr::select(-MP_bulk_wet, -MP_bulk_dry, -MP_avg) %>% 
 #   pivot_longer(cols=matches("MP[1-9]") 
 #                , names_to="MP"
 #                , values_to="MPa"
@@ -282,7 +289,7 @@ wp330pdind$MD_MPa <- NA
 
 
 #add in lat lon
-wp330 <- left_join(wp330pdind, latlon, by= c("Tag"="Tree"))
+#wp330 <- left_join(wp330pdind, latlon, by= c("Tag"="Tree"))
 #wp330all <- full_join(wp330pdind, wp330mdind)
 #wp330 <- full_join(wp330all, latlon, by= c("Tag"="Tree"))
 #wp330$Edrop <- wp330$MD_MPa - wp330$PD_MPa
@@ -297,9 +304,9 @@ wp330 <- left_join(wp330pdind, latlon, by= c("Tag"="Tree"))
 ####### ++ 4.04.22 #########
 wp44pd <- wpwc44 %>%
   filter(!is.na(Tag)) %>%
-  select(!matches("_g_")) %>% 
-  select(1:3,6,matches("PD")) %>% 
-  select(-PD_bulk_wet, -PD_bulk_dry, -PD_avg) %>% 
+  dplyr::select(!matches("_g_")) %>% 
+  dplyr::select(1:3,6,matches("PD")) %>% 
+  dplyr::select(-PD_bulk_wet, -PD_bulk_dry, -PD_avg) %>% 
   pivot_longer(cols=matches("PD[1-9]") 
                , names_to="PD"
                , values_to="MPa"
@@ -310,9 +317,9 @@ wp44pd$Date <- as_date("2022-04-04")
 
 wp44md <- wpwc44 %>%
   filter(!is.na(Tag)) %>%
-  select(!matches("_g_")) %>% 
-  select(1:3,6,matches("MD")) %>% 
-  select(-MD_bulk_wet, -MD_bulk_dry, -MD_avg) %>% 
+  dplyr::select(!matches("_g_")) %>% 
+  dplyr::select(1:3,6,matches("MD")) %>% 
+  dplyr::select(-MD_bulk_wet, -MD_bulk_dry, -MD_avg) %>% 
   pivot_longer(cols=matches("MD[1-9]") 
                , names_to="MD"
                , values_to="MPa"
@@ -332,7 +339,7 @@ wp44mdind$WOY <- week(wp44mdind$Date.MD)
 wp44all <- full_join(wp44pdind, wp44mdind, by=c("Tag", "WOY"))
 
 
-wp44 <- left_join(wp44all, latlon, by= c("Tag"="Tree"))
+#wp44 <- left_join(wp44all, latlon, by= c("Tag"="Tree"))
 #wp44$Edrop <- wp44$MD_MPa - wp44$PD_MPa
 
 # apply(wp44, FUN=function(x){ length(which(is.na(x)))}, MARGIN =2)
@@ -343,9 +350,9 @@ wp44 <- left_join(wp44all, latlon, by= c("Tag"="Tree"))
 ####### ++ 4.11.22 #########
 wp411pd <- wpwc411 %>%
   filter(!is.na(Tag)) %>%
-  select(!matches("_g_")) %>% 
-  select(1:3,6,matches("PD")) %>% 
-  select(-PD_bulk_wet, -PD_bulk_dry, -PD_avg) %>% 
+  dplyr::select(!matches("_g_")) %>% 
+  dplyr::select(1:3,6,matches("PD")) %>% 
+  dplyr::select(-PD_bulk_wet, -PD_bulk_dry, -PD_avg) %>% 
   pivot_longer(cols=matches("PD[1-9]") 
                , names_to="PD"
                , values_to="MPa"
@@ -356,9 +363,9 @@ wp411pd$Date <- as_date("2022-04-11")
 # currently no MD values
 # wp411md <- wpwc411 %>%
 #   filter(!is.na(Tag)) %>%
-#   select(!matches("_g_")) %>% 
-#   select(1:3,6,matches("MP")) %>% 
-#   select(-MP_bulk_wet, -MP_bulk_dry, -MP_avg) %>% 
+#   dplyr::select(!matches("_g_")) %>% 
+#   dplyr::select(1:3,6,matches("MP")) %>% 
+#   dplyr::select(-MP_bulk_wet, -MP_bulk_dry, -MP_avg) %>% 
 #   pivot_longer(cols=matches("MP[1-9]") 
 #                , names_to="MP"
 #                , values_to="MPa"
@@ -380,7 +387,7 @@ wp411pdind$MD_MPa <- NA
 
 
 #add in lat lon
-wp411 <- left_join(wp411pdind, latlon, by= c("Tag"="Tree"))
+#wp411 <- left_join(wp411pdind, latlon, by= c("Tag"="Tree"))
 #wp411all <- full_join(wp411pdind, wp411mdind)
 #wp411 <- full_join(wp411all, latlon, by= c("Tag"="Tree"))
 #wp411$Edrop <- wp411$MD_MPa - wp411$PD_MPa
@@ -392,7 +399,9 @@ wp411 <- left_join(wp411pdind, latlon, by= c("Tag"="Tree"))
 
 ################# Combine Ind average WPs together ###########
 
-wp.ind <- rbind(wp228,wp38, wp315, wp330, wp44, wp411)
+wp.ind <- rbind(wp228all,wp38all, wp315all, wp330pdind, wp44all, wp411pdind)
+wp.ind$Tag <- as.character(as.numeric(wp.ind$Tag))
+wp.ind <- left_join(wp.ind, latlon, by=c("Tag"="Tree"))
 wp.ind <- wp.ind[!is.na(wp.ind$Latitude),] 
 wp.ind$Site <- treesite$Site[match(wp.ind$Tag, treesite$Tag)]
 wp.ind$Plot <- treesite$Plot[match(wp.ind$Tag, treesite$Tag)]
