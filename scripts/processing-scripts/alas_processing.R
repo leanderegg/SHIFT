d71db1c2ccd47ed958c1fcfbdf343a40a6b3b2ba
bovingigit config --global user.name Jane <indraboving@gmail.com>
2023-01-30 09:36:48 -0800

    alas madness (matching areas to stems isn't going so well)

--- scripts/processing-scripts/alas_processing.R
+++ scripts/processing-scripts/alas_processing.R
@@ -8,6 +8,12 @@ library(readxl)
 library(gridExtra)
 library(MetBrewer)
 
+###LIT VALUES for QUDO:
+
+#SLA: 7.6 mm2/mg
+#stem diameter: 3.75mm
+#Huber value: 0.077 mm2/cm2
+
 alas_df_raw <- read_excel(here(dataversion,"WP_WC", "SHIFT data collection 2022.xlsx"), sheet="ALAS DATA", skip=0, na = "NA") %>% 
   clean_names() %>% 
   mutate(date = ymd(date), 
@@ -15,21 +21,47 @@ alas_df_raw <- read_excel(here(dataversion,"WP_WC", "SHIFT data collection 2022.
          #length_mm = case_when
          ) 
 
+unique(alas_df_raw$date)
+
 alas_df <- alas_df_raw %>% 
-  mutate(area_cm = ((area_scanned_cm/sdm_g)*ldm_g), 
-         area_mm = area_cm/10,
+   mutate(
+     #area_cm = ((area_scanned_cm/sdm_g)*ldm_g), 
+  #        area_mm = area_cm/10,
+  #        dm_g = sdm_g + ldm_g,
+  #        dm_mg = dm_g/1000,
+  #        lma = (sdm_g + ldm_g)/area_cm, 
+  #        d_cm = d_mm/10,
+  #        r_cm = d_cm/2,
+  #        area_stem_cm = (pi*(r_cm)^2), 
+  #        alas = (area_mm/area_stem_cm), 
+  #        sla = area_mm/(dm_mg), 
+         tree_id = as.factor(tree_id), 
+         branch = as.factor(branch), 
+         date_alas = date, 
+         year_alas = year, 
+         week = week(date)
+         ) %>% 
+#drop_na(alas) %>% 
+  select(-date, 
+         -year
+         )
+
+unique(alas_df$date_alas)
+
+#read in leaf areas: 
+leaf_area_df <- read_csv(here("processed-data", paste0("leaf_area_alldates_",datver,".csv")), show_col_types = FALSE) 
+##Combine: 
+
+alas_leaf_area_df_week <- merge(leaf_area_df, alas_df, by = c("tree_id", "branch", "species", "week")) %>% 
+  mutate(area_cm2_total = ((area_cm2/sdm_g)*ldm_g), 
+         area_mm2 = area_cm2_total*10,
          dm_g = sdm_g + ldm_g,
-         dm_mg = dm_g/1000,
-         lma = (sdm_g + ldm_g)/area_cm, 
+         dm_mg = dm_g*1000,
+         lma = (sdm_g + ldm_g)/area_cm2, 
          d_cm = d_mm/10,
          r_cm = d_cm/2,
-         area_stem_cm = (pi*(r_cm)^2), 
-         alas = (area_mm/area_stem_cm), 
-         sla = area_mm/(dm_mg)) %>% 
-drop_na(alas)
+         area_stem_cm2 = (pi*(r_cm)^2), 
+         alas = (area_mm2/area_stem_cm2), 
+         sla_mm_mg = area_mm2/(dm_mg))
 
-#Lit values: 
 
-#SLA: 7.6 mm2/mg
-#stem diameter: 3.75mm
-#Huber value: 0.077 mm2/cm2
--- scripts/processing-scripts/leaf_area_processing.R
+++ scripts/processing-scripts/leaf_area_processing.R
@@ -13,57 +13,102 @@ library(here)
 #dataversion <- paste0("Data_", datver)
 
 # all csv files
-alas_data_csv <- list.files(path = here("data", "leaf_area_scans"),  # Identify all CSV files
+leaf_area_data_csv <- list.files(path = here("data", "leaf_area_scans"),  # Identify all CSV files
                          pattern = "*.csv", full.names = TRUE, include.dirs = T) %>% 
     lapply(read_csv) %>%                              # Store all files in list
     bind_rows 
 
-alas_data_csv2 <- alas_data_csv %>%                                        # Combine data sets into one data set 
+leaf_area_data_csv2 <- leaf_area_data_csv %>%                                        # Combine data sets into one data set 
   clean_names() %>% 
   mutate(id = str_remove(slice, ".jpg"),
          id = str_remove(id, ".png"),
          area = total_area, 
          file = "csv", 
-         id = gsub(pattern="_", replacement="-", id, fixed = TRUE)) %>% 
+         id = gsub(pattern="_", replacement="-", id, fixed = TRUE),
+         id = gsub(pattern=".b", replacement="-b", id, fixed = TRUE)) %>% 
   select(id, area, file) 
 
 
 # all txt files
   
-alas_data_files_txt <- list.files(path = here("data", "leaf_area_scans"),  # Identify all txt files
+leaf_area_data_files_txt <- list.files(path = here("data", "leaf_area_scans"),  # Identify all txt files
                          pattern = "*.txt", full.names = TRUE, include.dirs = T)
 
-alas_data_files_txt_tables <- alas_data_files_txt %>% #read in all .txt files as a table
+leaf_area_data_files_txt_tables <- leaf_area_data_files_txt %>% #read in all .txt files as a table
                         #  lapply(read_delim)
                          lapply(read_table)  
 
-alas_with_names <- mapply(cbind, alas_data_files_txt_tables, "source"= alas_data_files_txt, SIMPLIFY = FALSE) #get the names of the files
+leaf_area_with_names <- mapply(cbind, leaf_area_data_files_txt_tables, "source"= leaf_area_data_files_txt, SIMPLIFY = FALSE) #get the names of the files
 
-alas_data_txt  <- data.table::rbindlist(alas_with_names, fill= T) #this will bind columns of different class
+leaf_area_data_txt  <- data.table::rbindlist(leaf_area_with_names, fill= T) #this will bind columns of different class
 
 
-alas_data_txt2 <- alas_data_txt %>% 
+leaf_area_data_txt2 <- leaf_area_data_txt %>% 
   clean_names() %>% 
-  mutate(id = str_remove(source,
-                         "/Users/user/Desktop/github/SHIFT/data/leaf_area_scans/"),
-         id = str_remove(id, ".txt"
-        ),
-        id = gsub(pattern="_", replacement="-", id, fixed = TRUE),
-        file = "txt"
+  mutate(id = str_remove(source,"/Users/user/Desktop/github/SHIFT/data/leaf_area_scans/"),
+         id = str_remove(id, ".txt"),
+         id = gsub(pattern="_", replacement="-", id, fixed = TRUE),
+         id = gsub(pattern=".b", replacement="-b", id, fixed = TRUE),
+         id = gsub(pattern=".Y", replacement="-Y", id, fixed = TRUE),
+         id = gsub(pattern="a-", replacement="-a-", id, fixed = TRUE),
+         id = gsub(pattern="b-", replacement="-b-", id, fixed = TRUE),
+         id = gsub(pattern="c-", replacement="-c-", id, fixed = TRUE),
+         id = gsub(pattern=".y", replacement="-y", id, fixed = TRUE),
+         id = gsub(pattern="bo", replacement="b0", id, fixed = TRUE),
+         file = "txt"
          ) %>% 
   select(id, area, file)
 
 #Combine: 
 
-alas_df <- rbind(alas_data_txt2, alas_data_csv2) %>% 
+leaf_area_df0 <- rbind(leaf_area_data_txt2, leaf_area_data_csv2) %>% 
   separate(id, sep = c("-", "_"),
-           into = c("spp", "tree", "date", "branch", "year", "extra_info", "more_info")) %>% 
+           into = c("spp", "tree", "date", "branch", "year", "extra_info", "more_info")) 
+
+leaf_area_df1 <- leaf_area_df0 %>% 
   mutate(date_new = mdy(date), 
          week = week(date_new), 
          day = day(date_new), 
          date_old = date, 
-         date = date_new) %>% 
-  select(-date_new)
+         date = date_new, 
+         branch = str_remove(branch, "b"),
+         branch = str_remove(branch, "B"),
+         year = str_remove(year, "Y"),
+         year = str_remove(year, "y"),
+         branch = str_remove(year, "y"),
+         tree_id = as.factor(tree), 
+         notes_leaf_area = extra_info, 
+         more_notes_leaf_area = more_info, 
+         area_cm2 = area, 
+         species = spp,
+         branch = gsub(pattern = "a", replacement = "1", branch, fixed = T),
+         branch = gsub(pattern = "b", replacement = "2", branch,fixed = T),
+         branch = gsub(pattern = "c", replacement = "3", branch,fixed = T),
+         branch = gsub(pattern = "o", replacement = "0", branch,fixed = T),
+         branch = gsub(pattern = "0 (2)", replacement = "0", branch, fixed = T),
+        # branch = gsub(pattern = "", replacement = NA_real_, branch, fixed = T),
+         year = gsub(pattern = "a", replacement = "1", year, fixed = T),
+         year = gsub(pattern = "b", replacement = "2", year,fixed = T),
+         year = gsub(pattern = "c", replacement = "3", year,fixed = T),
+         year = gsub(pattern = "o", replacement = "0", year,fixed = T),
+         year = gsub(pattern = "0 (2)", replacement = "0", year, fixed = T),
+         year = as.factor(year), 
+         date_leaf_area = date,
+         year_leaf_area = year,
+         week = week(date)
+         ) %>% 
+  select(-date_new, -extra_info, -more_info, -tree, -area, -spp, -date, 
+         -year, -date_old
+         )
+
+unique(leaf_area_df1$branch)
+unique(leaf_area_df1$year)
+
+date_weird <- leaf_area_df1 %>% 
+  filter(is.na(date))
+  
+
+write.csv(leaf_area_df1, here("processed-data", paste0("leaf_area_alldates_",datver,".csv")))
 
 #####
 #Try bluebs!
