7debdffc6ca23352d187e6cb67b92d6ada2a492d
bovingigit config --global user.name Jane <indraboving@gmail.com>
2023-02-20 07:21:17 -0800

    updating data

--- alas_processing.R
+++
@@ -1,8 +0,0 @@
-####Al:As processing####
-
-#Enter data
-alas_raw <- read_excel(here(dataversion,"WP_WC", "SHIFT data collection 2022.xlsx"), sheet="ALAS DATA", skip=0, na = "NA") %>% 
-  clean_names() 
-
-alas_df <- alas_raw %>% 
-  mutate(lma = )
