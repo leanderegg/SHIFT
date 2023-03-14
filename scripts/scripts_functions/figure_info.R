b15fcaef153561c8e1c362b0261174ff0f27c129
bovingigit config --global user.name Jane <indraboving@gmail.com>
2023-03-14 07:43:59 -0700

    trying to commit big files

--- scripts/scripts_functions/figure_info.R
+++ scripts/scripts_functions/figure_info.R
@@ -8,6 +8,10 @@ ggplot <- function(...) { ggplot2::ggplot(...) +
           panel.grid.minor = element_blank())  # Hide minor gridlines
 }
 
+Cross = list(c("#c969a1", "#ce4441", "#ee8577", "#eb7926", "#ffbb44", "#859b6c", "#62929a", "#004f63", "#122451"), 
+             c(4, 7, 1, 8, 2, 6, 3, 5, 9), 
+             colorblind=FALSE)
+
 nb.cols <- 58
 pal <- met.brewer("Cross")
 mycolors <- colorRampPalette(pal)(nb.cols)
